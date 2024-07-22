7d749956e9adbfeabcdb571ac5abc073a4e0a333
SimonWels <wels_freinsheim@web.de>
2024-07-22 09:31:15 +0200

    01 / Shape() + Rectangle

---
+++ com/cc/java/Rectangle.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Rectangle extends shape{
+
+    private double height, width; 
+
+    public Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+
+
+}
---
+++ com/cc/java/Shape.java
@@ -0,0 +1,10 @@
+package com.cc.java;
+
+public abstract ckass shape {
+    
+
+    public abstract double area();
+    
+
+
+}
